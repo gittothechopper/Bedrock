0973ba52b15425054d206c8f0b9b3c0cd88762b4
Alex Meah <hi@alexmeah.com>
2013-10-17 22:44:57 +0100

    Add sanitation of provided name, formatting.

--- tasks/create.js
+++ tasks/create.js
@@ -7,32 +7,33 @@ module.exports = function (grunt) {
 
     var name = grunt.option('name') || null;
 
-    grunt.registerTask('create', function() {
+    grunt.registerTask('create', function () {
 
-        var assetsDirectory = 'app/assets/',
+        var title = name,
+            filename = name.toUpperCase().split(' ').join('_'),
             pagesDirectory = 'app/templates/pages/',
-            cssDirectory = 'app/assets/styles/',
+            cssDirectory = 'app/assets/styles/pages/',
             imagesDirectory = 'app/assets/img/',
             javascriptDirectory = 'app/assets/scripts/',
-            modulePattern = 'var '+name+' = (function () {\n\tfunction init () {\n\t}\n\treturn {\n\t\tinit: init\n\t}\n}());\n\n$(function () {\n\t'+name+'.init();\n});',
-            templateContent = '---\ntitle: '+name+'\ncssname: ' + name + '\njsname: ' + name + '\n---';
+            modulePattern = 'var ' + name + ' = (function () {\n\tfunction init () {\n\t}\n\treturn {\n\t\tinit: init\n\t}\n}());\n\n$(function () {\n\t' + name + '.init();\n});',
+            templateContent = '---\ntitle: ' + title + '\ncssname: ' + filename + '\njsname: ' + filename + '\n---';
 
         // Cursory check that the file doesn't exist
-        if(grunt.file.exists(pagesDirectory+name+'.hbs')) {
-            grunt.fail.warn('Sorry, that page already exists' ,1);
+        if (grunt.file.exists(pagesDirectory + filename + '.hbs')) {
+            grunt.fail.warn('Sorry, that page already exists', 1);
         }
 
         // Make img directory
-        grunt.file.mkdir(imagesDirectory+name);
+        grunt.file.mkdir(imagesDirectory + filename);
 
         // create css
-        grunt.file.write(cssDirectory+name+'.scss', '#'+name+' {\n\n}');
+        grunt.file.write(cssDirectory + filename + '.scss', '#' + filename + ' {\n\n}');
 
         // create js module
-        grunt.file.write(javascriptDirectory+name+'.js', modulePattern)
+        grunt.file.write(javascriptDirectory + filename + '.js', modulePattern);
 
         // create page template
-        grunt.file.write(pagesDirectory+name+'.hbs', templateContent);
+        grunt.file.write(pagesDirectory + filename + '.hbs', templateContent);
 
     });
 };
